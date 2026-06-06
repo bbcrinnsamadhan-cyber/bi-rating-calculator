a60a81f93bf4de604110bb770244f9502606c566
bbcrinnsamadhan-cyber <bbc.rinnsamadhan@gmail.com>
2026-06-06 11:04:25 +0530

    Update api.js
    new

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,16 +1,29 @@
+// import axios from "axios";
+
+// // 🔹 Local backend (development)
+// const LOCAL_API = "http://localhost:5000";
+
+// // 🔹 Production backend (Render)
+// const PROD_API = "https://bi-rating-calculator.onrender.com";
+
+// // 🔹 Auto select
+// const API_BASE_URL =
+//   import.meta.env.MODE === "development"
+//     ? LOCAL_API
+//     : PROD_API;
+
+// export const submitLead = (data) => {
+//   return axios.post(`${API_BASE_URL}/api/leads`, data);
+// };
+
+
+
+
+
 import axios from "axios";
 
-// 🔹 Local backend (development)
-const LOCAL_API = "http://localhost:5000";
-
-// 🔹 Production backend (Render)
-const PROD_API = "https://bi-rating-calculator.onrender.com";
-
-// 🔹 Auto select
-const API_BASE_URL =
-  import.meta.env.MODE === "development"
-    ? LOCAL_API
-    : PROD_API;
+//export const API_BASE_URL = "http://localhost:5000";
+ export const API_BASE_URL = "https://bi-rating-calculator2.onrender.com";
 
 export const submitLead = (data) => {
   return axios.post(`${API_BASE_URL}/api/leads`, data);
